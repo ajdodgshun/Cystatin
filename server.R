5a1490156289c8c19b68657498645efca57b1ba2
ajdodgshun <ajdodgshun@gmail.com>
2015-05-14 15:44:57 +1000

    Updated error message

--- server.R
+++ server.R
@@ -12,9 +12,10 @@ FILLERform <- function(cystatin) exp(1.962 + (1.123*(log(10/cystatin))))
 
 shinyServer(function(input, output) {
         numericsex <- reactive({
-                if(input$sex == "M") nsex <- "1"
-                else nsex <- "0"
-                print(as.numeric(nsex))
+                if(input$sex %in% c("M", "m")) nsex <- "1"
+                else if(input$sex %in% c("F", "f")) nsex <- "0"
+                else nsex <- "NA"
+                as.numeric(nsex)
         })
         output$filler <- renderText({
                 if(input$goButton == 0) ""
@@ -30,7 +31,10 @@ recommend <- reactive({
         recnewform <- NEWform(input$cysc, input$height, input$crt)
         recfiller <- FILLERform(input$cysc)
         recCKID <- CKIDform(numericsex(), input$height, input$crt, input$cysc, input$urea)
-        if(recCKID < 100 && recnewform < 100){
+        
+        if(recCKID < 20 || recCKID > 250){
+                rec <- "Error in inputs, please check values and units carefully"
+        }else if(recCKID < 100 && recnewform < 100){
                 rec <- "Recommend formal isotopic GFR, reasonable likelihood GFR is less than 90"
         } else if(recCKID < 100 && recnewform >=100) {
                 rec <- "Disagreement between formulae, consider formal GFR if clinically appropriate"
@@ -39,7 +43,7 @@ recommend <- reactive({
         } else if(recCKID >= 100 && recnewform >= 100) {
                 rec <- "GFR is likely to be greater than 90mL/min/1.73m2"
         } else rec <- ""
-        print(rec)
+        return(rec)
 })
 output$recommendation <- renderText({
         if(input$goButton == 0) ""
